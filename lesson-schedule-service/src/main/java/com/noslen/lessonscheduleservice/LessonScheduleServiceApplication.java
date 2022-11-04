e4583e5c0309bb7ea46475a662e13e7e501f9590
ntjohns1 <nelsontjohns@gmail.com>
2022-11-04 16:40:00 -0400

    Added jdbc datasource to securityconfig

---
+++ lesson-schedule-service/src/main/java/com/noslen/lessonscheduleservice/LessonScheduleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.noslen.lessonscheduleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonScheduleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonScheduleServiceApplication.class, args);
+	}
+
+}
