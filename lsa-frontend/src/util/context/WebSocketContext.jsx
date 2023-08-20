d561d6cc95c8feadc4b2e703d8d1bf1b5cf4df83
ntjohns1 <nelsontjohns@gmail.com>
2023-08-20 00:12:20 -0400

    attempt to reconnect websocket if connection dies

--- lsa-frontend/src/util/context/WebSocketContext.jsx
+++ lsa-frontend/src/util/context/WebSocketContext.jsx
@@ -9,63 +9,85 @@ export function useSocket() {
 
 export const WebSocketProvider = ({ children }) => {
   const { authState, oktaAuth } = useOktaAuth();
-  const accessToken = oktaAuth.getAccessToken();
   const principle = authState && authState.idToken && authState.idToken.claims.name;
-  const claims = authState && authState.idToken && authState.idToken.claims;
-  console.log(claims);
   const [inMessage, setInMessage] = useState({});
+  const [reconnecting, setReconnecting] = useState(false);
   const webSocketRef = useRef(null);
+  const maxReconnectAttempts = 3;
+  const initialDelay = 2000;
 
   useEffect(() => {
     if (authState && authState.isAuthenticated) {
-      const socket = new WebSocket(config.resourceServer.socketUri + principle);
-      webSocketRef.current = socket;
-      socket.addEventListener('message', function (event) {
-        console.log("WebSocket message:", event);
-        // setInMessage(event.data)
-        setInMessage( JSON.parse(event.data));
-      });
-      socket.addEventListener("open", function (event) {
-        console.log("WebSocket connection opened:", event);
-      });
-
-      socket.addEventListener("close", function (event) {
-        if (event.wasClean) {
-          console.log("WebSocket connection closed cleanly:", event);
-        } else {
-          console.error("WebSocket connection died:", event);
-        }
-      });
-      
-      socket.addEventListener("error", function (event) {
-        console.error("WebSocket error:", event);
-      });
-      return () => {
-        socket.close();
-      }
+      connectSocket();
+    }
+    return () => {
+      socket.close();
     }
   }, [authState]);
 
-  useEffect(() => {
-    if (webSocketRef.current && authState && authState.isAuthenticated) {
-      // const interval = setInterval(() => {
-        if (webSocketRef.current.readyState === WebSocket.OPEN) {
-          webSocketRef.current.send('ping');
-        }
-      // }, 10000); 
-  
-      // return () => clearInterval(interval);
+  const connectSocket = () => {
+ 
+    const socket = new WebSocket(config.resourceServer.socketUri + principle);
+    webSocketRef.current = socket;
+    socket.addEventListener('message', function (event) {
+      console.log("WebSocket message:", event);
+      setInMessage(JSON.parse(event.data));
+    });
+    socket.addEventListener("open", function (event) {
+      console.log("WebSocket connection opened:", event);
+      socket.send('ping');
+    });
+
+    socket.addEventListener("close", function (event) {
+      if (event.wasClean) {
+        console.log("WebSocket connection closed cleanly:", event);
+      } else {
+        console.error("WebSocket connection died:", event);
+        console.log("Attempting to reconnect WebSocket");
+        setReconnecting(true);
+        reconnectSocket();
+      }
+    });
+
+    socket.addEventListener("error", function (event) {
+      console.error("WebSocket error:", event);
+    });
+  }
+
+
+  const reconnectSocket = (attempt = 1) => {
+    if (attempt > maxReconnectAttempts) {
+      console.error("Max reconnect attempts reached");
+      setReconnecting(false);
+      return;
     }
-  }, []);
+  
+    console.log(`Reconnect attempt ${attempt}`);
+  
+    if (webSocketRef.current.readyState === WebSocket.CLOSED) {
+      connectSocket();  
+  
+      setTimeout(() => {
+        if (webSocketRef.current.readyState === WebSocket.CLOSED) {
+          reconnectSocket(attempt + 1);
+        } else {
+          setReconnecting(false);
+        }
+      }, initialDelay * Math.pow(2, attempt - 1)); // Exponential backoff
+    } else {
+      setReconnecting(false);
+    }
+  };
   
 
-  const disconnect = () => webSocketRef.current.close();
+
+  const disconnectSocket = () => webSocketRef.current.close();
 
   return (
     <WebSocketContext.Provider value={{
       principle,
       inMessage,
-      disconnect
+      disconnectSocket
     }}>
       {children}
     </WebSocketContext.Provider>
