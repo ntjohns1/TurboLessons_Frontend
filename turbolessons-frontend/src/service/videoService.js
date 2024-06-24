a016cb60f66d1940ec2b0bfdf516380670d62f89
ntjohns1 <nelsontjohns@gmail.com>
2024-06-23 22:06:29 -0400

    update to video service

--- turbolessons-frontend/src/service/videoService.js
+++ turbolessons-frontend/src/service/videoService.js
@@ -1,22 +1,22 @@
-import api from './axiosConfig';
+import api from "./axiosConfig";
 
 export const fetchVideos = async () => {
   try {
-    const response = await api.get('/video');
+    const response = await api.get("/video");
     return response.data;
   } catch (error) {
-    console.error('Error fetching videos:', error);
+    console.error("Error fetching videos:", error);
     throw error;
   }
 };
 
 export const getVideo = async (videoSrc) => {
   try {
-    const response = await api.get(videoSrc, { responseType: 'blob' });
+    const response = await api.get(videoSrc, { responseType: "blob" });
     const url = window.URL.createObjectURL(response.data);
     return url;
   } catch (error) {
-    console.error('Error fetching video:', error);
+    console.error("Error fetching video:", error);
     throw error;
   }
 };
@@ -24,16 +24,16 @@ export const getVideo = async (videoSrc) => {
 export const uploadVideo = async (formState) => {
   try {
     const formData = new FormData();
-    formData.append('file', formState);
+    formData.append("file", formState);
 
-    const response = await api.post('/video', formData, {
+    const response = await api.post("/video", formData, {
       headers: {
-        'Content-Type': 'multipart/form-data',
-      }
+        "Content-Type": "multipart/form-data",
+      },
     });
     return response.data;
   } catch (error) {
-    console.error('Error uploading video:', error);
+    console.error("Error uploading video:", error);
     throw error;
   }
 };
