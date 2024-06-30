5d0470ff28fabf199d6ccfe054096a41d56dc2c1
ntjohns1 <nelsontjohns@gmail.com>
2024-06-29 21:21:49 -0400

    getVideo in PlayVideo.jsx

--- turbolessons-frontend/src/components/TeacherComponents/Videos/PlayVideo.jsx
+++ turbolessons-frontend/src/components/TeacherComponents/Videos/PlayVideo.jsx
@@ -1,33 +1,27 @@
 import React, { useEffect, useRef } from 'react';
 import { useOktaAuth } from '@okta/okta-react';
+import { getVideo } from '../../../service/videoService';
+import { setAccessToken } from '../../../service/axiosConfig';
 import config from '../../../config';
 
 const PlayVideo = ({ selected }) => {
     const videoRef = useRef();
     const videoSrc = `${config.resourceServer.videoUrl}/${selected.name}`;
     const { authState, oktaAuth } = useOktaAuth();
-    const accessToken = oktaAuth.getAccessToken();
 
     useEffect(() => {
-        if (!authState.isAuthenticated) {
-            // If the user is not authenticated, do nothing
-            return;
+        const fetchVideo = async () => {
+            try {
+                const accessToken = oktaAuth.getAccessToken();
+                setAccessToken(accessToken);
+                const url = await getVideo(videoSrc);
+                videoRef.current.src = url;
+            } catch (error) {
+                console.error(error);
+            }
         }
 
-        // Fetch video
-        fetch(videoSrc, {
-            
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            },
-        })
-            .then((res) => res.blob())
-            .then((blob) => {
-                const url = window.URL.createObjectURL(blob);
-                videoRef.current.src = url;
-            })
-            .catch((err) => console.error(err));
+        fetchVideo()
     }, [authState, videoSrc]);
 
     return <video ref={videoRef} controls style={{ width: "100%", height: "auto" }} />;
