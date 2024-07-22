afb7938f6ea029772e9e21b8fe8c4b8859a667f1
ntjohns1 <nelsontjohns@gmail.com>
2024-07-22 16:44:20 -0400

    debug onHide prop for closing LessonModal/LessonConfirm

--- turbolessons-frontend/src/components/TeacherComponents/Lessons/LessonView.jsx
+++ turbolessons-frontend/src/components/TeacherComponents/Lessons/LessonView.jsx
@@ -9,7 +9,7 @@ import config from '../../../config';
 import "react-datepicker/dist/react-datepicker.css";
 import LessonConfirm from './LessonConfirm';
 
-const LessonView = ({ event, setUpdate }) => {
+const LessonView = ({ event, setUpdate, onHide }) => {
 
     const [showConfirm, setShowConfirm] = useState(false)
     const handleCloseConfirm = () => setShowConfirm(false);
@@ -17,81 +17,82 @@ const LessonView = ({ event, setUpdate }) => {
 
     return (
         <Container>
-            <Card className='d-flex justify-content-center'>
-                <Form className="m-3 px-3">
-                    <Form.Group className="mb-3 px-3" controlId="selectStudent">
-                        <Form.Label>Student</Form.Label>
-                        <Form.Control plaintext readOnly defaultValue={event.student} />
-                    </Form.Group>
-                    <Form.Group className="mb-3 px-3" controlId="date">
-                        <Form.Label>Date</Form.Label>
-                        <DatePicker
-                            className='mx-3 px-3'
-                            selected={event.start}
-                            readOnly
-                            disabled
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3 px-3" controlId="formStartTime">
-                        <Form.Label>Start Time</Form.Label>
-                        <DatePicker
-                            className='mx-3 px-3'
-                            selected={event.start}
-                            showTimeSelect
-                            showTimeSelectOnly
-                            timeIntervals={15}
-                            timeCaption="Time"
-                            dateFormat="h:mm aa"
-                            readOnly
-                            disabled
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3 px-3" controlId="formEndTime">
-                        <Form.Label>End Time</Form.Label>
-                        <DatePicker
-                            className='mx-3 px-3'
-                            selected={event.end}
-                            showTimeSelect
-                            showTimeSelectOnly
-                            timeIntervals={15}
-                            timeCaption="Time"
-                            dateFormat="h:mm aa"
-                            disabled
-                            readOnly
-                        />
-                    </Form.Group>
+            {!showConfirm ? (
+                <Card className='d-flex justify-content-center'>
+                    <Form className="m-3 px-3">
+                        <Form.Group className="mb-3 px-3" controlId="selectStudent">
+                            <Form.Label>Student</Form.Label>
+                            <Form.Control plaintext readOnly defaultValue={event.student} />
+                        </Form.Group>
+                        <Form.Group className="mb-3 px-3" controlId="date">
+                            <Form.Label>Date</Form.Label>
+                            <DatePicker
+                                className='mx-3 px-3'
+                                selected={event.start}
+                                readOnly
+                                disabled
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3 px-3" controlId="formStartTime">
+                            <Form.Label>Start Time</Form.Label>
+                            <DatePicker
+                                className='mx-3 px-3'
+                                selected={event.start}
+                                showTimeSelect
+                                showTimeSelectOnly
+                                timeIntervals={15}
+                                timeCaption="Time"
+                                dateFormat="h:mm aa"
+                                readOnly
+                                disabled
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3 px-3" controlId="formEndTime">
+                            <Form.Label>End Time</Form.Label>
+                            <DatePicker
+                                className='mx-3 px-3'
+                                selected={event.end}
+                                showTimeSelect
+                                showTimeSelectOnly
+                                timeIntervals={15}
+                                timeCaption="Time"
+                                dateFormat="h:mm aa"
+                                disabled
+                                readOnly
+                            />
+                        </Form.Group>
 
-                    <Form.Group className="mb-3 px-3" controlId="comments">
-                        <Form.Label>Comments</Form.Label>
-                        <Form.Control plaintext readOnly defaultValue={event.comments} />
-                    </Form.Group>
-                </Form>
-                <Card.Footer className='d-flex'>
-                    <Button
-                        className="mx-3"
-                        variant="secondary"
-                        style={{ cursor: 'pointer' }}
-                        onClick={() => setUpdate(true)}
-                    >
-                        Edit
-                    </Button>
-                    <Button
-                        className="mx-3"
-                        variant="danger"
-                        style={{ cursor: 'pointer' }}
-                        onClick={handleShowConfirm}
-                    >
-                        Delete
-                    </Button>
-                    <LessonConfirm
-                        show={showConfirm}
-                        onHide={handleCloseConfirm}
-                        eventId={event.id}
-                        backdrop="static"
-                        keyboard="false"
-                    />
-                </Card.Footer>
-            </Card>
+                        <Form.Group className="mb-3 px-3" controlId="comments">
+                            <Form.Label>Comments</Form.Label>
+                            <Form.Control plaintext readOnly defaultValue={event.comments} />
+                        </Form.Group>
+                    </Form>
+                    <Card.Footer className='d-flex'>
+                        <Button
+                            className="mx-3"
+                            variant="secondary"
+                            style={{ cursor: 'pointer' }}
+                            onClick={() => setUpdate(true)}
+                        >
+                            Edit
+                        </Button>
+                        <Button
+                            className="mx-3"
+                            variant="danger"
+                            style={{ cursor: 'pointer' }}
+                            onClick={handleShowConfirm}
+                        >
+                            Delete
+                        </Button>
+                    </Card.Footer>
+                </Card>
+            ) : (
+                <LessonConfirm
+                    show={showConfirm}
+                    onHide={onHide}
+                    eventId={event.id}
+                />
+            )}
         </Container>
     );
 };
