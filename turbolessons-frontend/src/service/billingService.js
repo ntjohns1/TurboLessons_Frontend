de20c38b1ff90f91294362290ce17acb7bd8f35f
ntjohns1 <nelsontjohns@gmail.com>
2024-09-29 20:30:25 -0400

    create BillingSlice and BillingService, release planning

---
+++ turbolessons-frontend/src/service/billingService.js
@@ -0,0 +1,68 @@
+import api from "./axiosConfig";
+
+// Customer
+
+// route((GET("/api/customer")), handler::listAll)
+// route(GET("/api/customer/{id}"), handler::retrieve)
+// route(POST("/api/customer"), handler::create)
+// route(PUT("/api/customer/{id}"), handler::update)
+// route(DELETE("/api/customer/{id}"), handler::delete);
+
+
+// PaymentIntent
+
+// route((GET("/api/paymentintent")), handler::listAll)
+// route(GET("/api/paymentintent/{id}"), handler::retrieve)
+// route(GET("/api/paymentintent/customer/{id}"), handler::searchByCustomer)
+// route(POST("/api/paymentintent"), handler::create)
+// route(PUT("/api/paymentintent/{id}"), handler::update)
+// route(PUT("/api/paymentintent/capture/{id}"), handler::capture)
+// route(DELETE("/api/paymentintent/{id}"), handler::cancel);
+
+
+// PaymentMethod
+
+// route((GET("/api/paymentintent")), handler::listAll)
+// route(GET("/api/paymentintent/{id}"), handler::retrieve)
+// route(GET("/api/paymentintent/customer/{id}"), handler::searchByCustomer)
+// route(POST("/api/paymentintent"), handler::create)
+// route(PUT("/api/paymentintent/{id}"), handler::update)
+// route(PUT("/api/paymentintent/capture/{id}"), handler::capture)
+// route(DELETE("/api/paymentintent/{id}"), handler::cancel);
+
+
+// Price
+
+// route(GET("/api/price"), handler::listAll)
+// route(GET("/api/price/{id}"), handler::retrieve)
+// route(POST("/api/price"), handler::create)
+// route(PUT("/api/price/{id}"), handler::update);
+
+
+// Product
+
+// route((GET("/api/product")),handler::listAll)
+// route(GET("/api/product/{id}"),handler::retrieve)
+// route(POST("/api/product"),handler::create)
+// route(PUT("/api/product/{id}"),handler::update)
+// route(DELETE("/api/product/{id}"),handler::delete);
+
+
+// SetupIntent
+
+// route((GET("/api/setupintent")),handler::listAll)
+// route(GET("/api/setupintent/{id}"),handler::retrieve)
+// route(POST("/api/setupintent"),handler::create)
+// route(PUT("/api/setupintent/confirm/{id}"),handler::confirm)
+// route(PUT("/api/setupintent/{id}"),handler::update)
+// route(DELETE("/api/setupintent/{id}"),handler::cancel);
+
+
+// Subscription
+
+// route((GET("/api/subscription")), handler::listAll)
+// route(GET("/api/subscription/{id}"), handler::retrieve)
+// route(POST("/api/subscription/{priceId}"), handler::create)
+// route(PUT("/api/subscription/{id}"), handler::update)
+// route(DELETE("/api/subscription/{id}"), handler::cancel);
+
