aa3c7352c4d54fc3c994d86990da200ac62df2c6
ntjohns1 <nelsontjohns@gmail.com>
2024-10-13 11:15:41 -0400

    Created basic student slice

--- turbolessons-frontend/src/components/TeacherComponents/Students/StudentSlice.js
+++ turbolessons-frontend/src/components/TeacherComponents/Students/StudentSlice.js
@@ -1,26 +1,111 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-
+import {
+  createStudent,
+  deleteStudent,
+  editStudent,
+  getStudentProfile,
+  getStudentsByTeacher,
+} from "../../../service/adminService";
 // Thunks
 
+export const fetchTeacherStudents = createAsyncThunk(
+  "students/fetchTeacherStudents",
+  async ({ teacher }) => {
+    const response = await getStudentsByTeacher(teacher);
+    return response;
+  }
+);
 
+export const fetchStudentProfile = createAsyncThunk(
+  "students/fetchStudentProfile",
+  async ({ id }) => {
+    const response = await getStudentProfile(id);
+    return response;
+  }
+);
 
+export const createNewStudent = createAsyncThunk(
+  "students/createNewStudent",
+  async (studentData) => {
+    const response = await createStudent(studentData);
+    return response;
+  }
+);
+
+export const updateStudent = createAsyncThunk(
+  "students/updateStudent",
+  async ({ id, formState }) => {
+    const response = editStudent(id, formState);
+    return response;
+  }
+);
+
+export const removeStudent = createAsyncThunk(
+  "students/deleteStudent",
+  async (id) => {
+    const response = await deleteStudent(id);
+    return response;
+  }
+);
 
 // Slice
 const studentSlice = createSlice({
-    name: "students",
-    initialState: {
-
+  name: "students",
+  initialState: {
+    studentsByTeacher: [],
+    // selectedStudent: null,
+    selectedProfile: null,
+    loading: false,
+    studentFetchFailed: false,
+  },
+  reducers: {
+    setSelectedProfile(state, action) {
+      state.selectedProfile = action.payload;
     },
-    reducers: {
-    
+    setLoading(state, action) {
+      state.loading = action.payload;
     },
-    extraReducers: (builder) => {
-
+    setStudentFetchFailed(state, action) {
+      state.studentFetchFailed = action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTeacherStudents.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchTeacherStudents.fulfilled, (state, action) => {
+        state.loading = false;
+        state.studentFetchFailed = false;
+        state.studentsByTeacher = action.payload;
+      })
+      .addCase(fetchTeacherStudents.rejected, (state) => {
+        state.loading = false;
+        state.studentFetchFailed = true;
+      })
+      .addCase(createNewStudent.fulfilled, (state, action) => {
+        state.studentsByTeacher.push(action.payload);
+      })
+      .addCase(updateStudent.fulfilled, (state, action) => {
+        const index = state.studentsByTeacher.findIndex(
+          (student = student.id === action.payload.id)
+        );
+        if (index !== -1) {
+          state.studentsByTeacher[index] = action.payload;
+        }
+      })
+      .addCase(deleteStudent.fulfilled, (state, action) => {
+        state.studentsByTeacher = state.studentsByTeacher.filter(
+          (student) => student.id !== action.payload
+        );
+      });
+  },
 });
 
-export const {
+export const { setSelectedProfile, setLoading, setStudentFetchFailed } =
+  studentSlice.actions;
 
-} = studentSlice.actions;
+export const selectStudentById = (state, studentId) =>
+    state.students.studentsByTeacher.find((student) => student.id === studentId);
 
 export default studentSlice.reducer;
