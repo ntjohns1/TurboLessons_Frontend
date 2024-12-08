5186795c322c51b2cc61a4c538b4850be3887e2f
ntjohns1 <nelsontjohns@gmail.com>
2024-12-08 12:27:00 -0500

    removed StudentContext

--- turbolessons-frontend/src/App.jsx
+++ turbolessons-frontend/src/App.jsx
@@ -6,7 +6,6 @@ import { Security } from '@okta/okta-react';
 import { LoginCallback } from '@okta/okta-react';
 import Loading from './helpers/Loading';
 import config from './config';
-import { StudentProvider } from './util/context/StudentContext';
 import { WebSocketProvider } from "./util/context/WebSocketContext.jsx";
 import { Route, Routes } from "react-router-dom";
 import TeacherDashboard from './pages/Teachers/TeacherDashboard';
@@ -23,7 +22,6 @@ import TeacherLayoutWrapper from './layouts/TeacherLayoutWrapper'
 import './App.css';
 import ManageSubscription from './components/TeacherComponents/Billing/ManageSubscription.jsx';
 
-
 const oktaAuth = new OktaAuth(config.oidc);
 
 const App = () => {
@@ -34,46 +32,44 @@ const App = () => {
 
   return (
     <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
-      <StudentProvider>
-        <WebSocketProvider>
-          <Routes>
-            <Route path="login/callback" element={<LoginCallback loadingElement={<Loading />} />} />
-            <Route path="/" element={<RequiredAuth />}>
-              <Route path="" element={<TeacherLayoutWrapper component={TeacherDashboard} />} />
-            </Route>
-            <Route path="/students" element={<RequiredAuth />}>
-              <Route path="" element={<TeacherLayoutWrapper component={Students} /> } />
-            </Route>
-            <Route path="/students/:id" element={<RequiredAuth />}>
-              <Route path="" element={<TeacherLayoutWrapper component={SingleStudent} />} />
-            </Route>
-            <Route path="/students/:id/billing" element={<RequiredAuth />}>
-              <Route path="" element={<TeacherLayoutWrapper component={ManageSubscription} />} />
-            </Route>
-            <Route path="/addStudent" element={<RequiredAuth />}>
-              <Route path="" element={<TeacherLayoutWrapper component={AddStudent} />} />
-            </Route>
-            <Route path="/calendar" element={<RequiredAuth />}>
-              <Route path="" element={<TeacherLayoutWrapper component={LessonCalendar} />} />
-            </Route>
-            <Route path="/messages" element={<RequiredAuth />}>
-            <Route path="" element={<TeacherLayoutWrapper component={Messenger} /> } />
-            </Route>
-            <Route path="/lessons" element={<RequiredAuth />}>
-              <Route path="" element={<TeacherLayoutWrapper component={Lessons} />} />
-            </Route>
-            <Route path="/videos" element={<RequiredAuth />}>
-              <Route path="" element={<TeacherLayoutWrapper component={Videos} />} />
-            </Route>
-            <Route path="/studentHome" element={<RequiredAuth />}>
-              <Route path="" element={<Videos />} />
-            </Route>
-            <Route path="/unauthorized" element={<RequiredAuth />}>
-              <Route path="" element={<Unauthorized />} />
-            </Route>
-          </Routes>
-        </WebSocketProvider>
-      </StudentProvider>
+      <WebSocketProvider>
+        <Routes>
+          <Route path="login/callback" element={<LoginCallback loadingElement={<Loading />} />} />
+          <Route path="/" element={<RequiredAuth />}>
+            <Route path="" element={<TeacherLayoutWrapper component={TeacherDashboard} />} />
+          </Route>
+          <Route path="/students" element={<RequiredAuth />}>
+            <Route path="" element={<TeacherLayoutWrapper component={Students} />} />
+          </Route>
+          <Route path="/students/:id" element={<RequiredAuth />}>
+            <Route path="" element={<TeacherLayoutWrapper component={SingleStudent} />} />
+          </Route>
+          <Route path="/students/:id/billing" element={<RequiredAuth />}>
+            <Route path="" element={<TeacherLayoutWrapper component={ManageSubscription} />} />
+          </Route>
+          <Route path="/addStudent" element={<RequiredAuth />}>
+            <Route path="" element={<TeacherLayoutWrapper component={AddStudent} />} />
+          </Route>
+          <Route path="/calendar" element={<RequiredAuth />}>
+            <Route path="" element={<TeacherLayoutWrapper component={LessonCalendar} />} />
+          </Route>
+          <Route path="/messages" element={<RequiredAuth />}>
+            <Route path="" element={<TeacherLayoutWrapper component={Messenger} />} />
+          </Route>
+          <Route path="/lessons" element={<RequiredAuth />}>
+            <Route path="" element={<TeacherLayoutWrapper component={Lessons} />} />
+          </Route>
+          <Route path="/videos" element={<RequiredAuth />}>
+            <Route path="" element={<TeacherLayoutWrapper component={Videos} />} />
+          </Route>
+          <Route path="/studentHome" element={<RequiredAuth />}>
+            <Route path="" element={<Videos />} />
+          </Route>
+          <Route path="/unauthorized" element={<RequiredAuth />}>
+            <Route path="" element={<Unauthorized />} />
+          </Route>
+        </Routes>
+      </WebSocketProvider>
     </Security>
   );
 };
