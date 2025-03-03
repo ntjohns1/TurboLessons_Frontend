0676a8d8fb3db18b8fc5a02edf151353a0ef42a9
ntjohns1 <nelsontjohns@gmail.com>
2025-03-03 13:58:10 -0500

    Created LogLesson component for creating Lesson MeterEvents

--- turbolessons-frontend/src/components/TeacherComponents/Lessons/LessonList.jsx
+++ turbolessons-frontend/src/components/TeacherComponents/Lessons/LessonList.jsx
@@ -1,28 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Toast, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { Card, Table, Toast, Button } from 'react-bootstrap';
 import { fetchEventsByTeacher } from '../../../service/eventService';
 import LessonForm from './LessonForm';
+import LogLesson from '../Billing/LogLesson';
 import { useOktaAuth } from '@okta/okta-react';
 
 
 const LessonList = () => {
-    const [lessons, setLessons] = useState([]);
-    const [editLesson, setEditLesson] = useState(null);
+    // const [lessons, setLessons] = useState([]);
+    // const [editLesson, setEditLesson] = useState(null);
     const { authState, oktaAuth } = useOktaAuth();
     const principle = authState && authState.idToken && authState.idToken.claims.name;
+    const dispatch = useDispatch();
+    const editLesson = useSelector((state) => state.lessons.isUpdate);
+    const setEditLesson = (lesson) => dispatch({ type: 'lessons/setEditLesson', payload: lesson });
+    const lessons = useSelector((state) => state.lessons.eventsByTeacher);
 
 
     useEffect(() => {
         const loadLessons = async () => {
             try {
                 const data = await fetchEventsByTeacher(principle);
-                setLessons(data);
             } catch (error) {
                 console.error('Error fetching lessons:', error);
             }
         };
-
-        loadLessons();
+        if (!lessons.length) {
+            loadLessons();
+        }
     }, []);
 
     const handleEdit = (lesson) => {
@@ -34,31 +40,47 @@ const LessonList = () => {
         const loadLessons = async () => {
             try {
                 const data = await fetchEventsByTeacher(principle);
-                setLessons(data);
             } catch (error) {
                 console.error('Error fetching lessons:', error);
             }
         };
-
         loadLessons();
     };
 
     return (
-        <Card>
-            {editLesson ? (
-                <LessonForm event={editLesson} handleSave={handleSave}/>
-            ) : (
-                <Card.Body>
-                    {lessons && lessons.map(lesson => (
-                        <Toast onClick={() => handleEdit(lesson)} key={lesson.id}>
-                            <Toast.Header closeButton={false}>
-                                <strong className="me-auto">{lesson.title} {lesson.date} {lesson.time}</strong>
-                            </Toast.Header>
-                        </Toast>
-                    ))}
-                </ Card.Body>
-            )}
-        </ Card>
+        <div style={{ height: '90vh' }}>
+            <Card >
+                {editLesson ? (
+                    <LessonForm event={editLesson} handleSave={handleSave} />
+                ) : (
+                    <Card.Body style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+                        <div style={{ flexGrow: 1, overflowY: 'auto', maxHeight: '70vh' }}>
+                            <Table striped bordered hover>
+                                <thead>
+                                    <tr>
+                                        <th>Date</th>
+                                        <th>Title</th>
+                                        <th>Log Lesson</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {lessons &&
+                                        [...lessons]
+                                            .sort((a, b) => new Date(b.date) - new Date(a.date)) // Sorting by date
+                                            .map((lesson) => (
+                                                <tr onClick={() => handleEdit(lesson)} key={lesson.id}>
+                                                    <td>{lesson.date}</td>
+                                                    <td>{lesson.title}</td>
+                                                    <td><LogLesson /></td>
+                                                </tr>
+                                            ))}
+                                </tbody>
+                            </Table>
+                        </div>
+                    </ Card.Body>
+                )}
+            </ Card>
+        </div>
     );
 };
 
--- turbolessons-frontend/src/pages/Teachers/Lessons.jsx
+++ turbolessons-frontend/src/pages/Teachers/Lessons.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import LessonForm from '../../components/TeacherComponents/Lessons/LessonForm.jsx'
+import LessonList from '../../components/TeacherComponents/Lessons/LessonList.jsx';
 
 export default function Lessons() {
     return (
         <Container>
-            <LessonForm/>
+            {/* <LessonForm/> */}
+            <LessonList/>
         </Container>
     )
 };
