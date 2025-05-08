f46beea2b9d63c31bd123968ae3065ab8951d52f
ntjohns1 <nelsontjohns@gmail.com>
2025-05-08 13:47:58 -0400

    Add auth token handling and subscription data fetching to Payments pages

--- turbolessons-frontend/src/components/StudentComponents/Payments/SubscriptionDetail.jsx
+++ turbolessons-frontend/src/components/StudentComponents/Payments/SubscriptionDetail.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { useOktaAuth } from "@okta/okta-react";
+import { setAccessToken } from "../../../service/axiosConfig";
+import { fetchAllPricesThunk, fetchAllProductsThunk, fetchOneSubscriptionThunk } from "../Payments/PaymentsSlice";
 import { Card } from "react-bootstrap";
 
 const SubscriptionDetail = ({ subscription }) => {
     const dispatch = useDispatch();
+    const { oktaAuth } = useOktaAuth();
     const productAdapter = useSelector((state) => state.payments.entities["products"]);
     const priceAdapter = useSelector((state) => state.payments.entities["prices"]);
     const product = Object.values(productAdapter.entities).find(p => p.id === subscription?.items?.data[0].price.product);
@@ -12,13 +16,15 @@ const SubscriptionDetail = ({ subscription }) => {
     const startDate = periodStart ? new Date(periodStart * 1000).toLocaleDateString() : 'N/A';
     const periodEnd = subscription?.currentPeriodEnd;
     const endDate = periodEnd ? new Date(periodEnd * 1000).toLocaleDateString() : 'N/A';
+    
     useEffect(() => {
         if (!productAdapter.ids.length || !priceAdapter.ids.length) {
+            setAccessToken(oktaAuth.getAccessToken());
             dispatch(fetchAllPricesThunk());
             dispatch(fetchAllProductsThunk());
 
         }
-    }, [productAdapter, priceAdapter]);
+    }, [productAdapter, priceAdapter, dispatch, oktaAuth]);
 
     return (
         <Card className="m-2">
--- turbolessons-frontend/src/pages/Students/Payments.jsx
+++ turbolessons-frontend/src/pages/Students/Payments.jsx
@@ -1,29 +1,78 @@
-import React from 'react';
-import { Container, Modal, Row, Col } from 'react-bootstrap';
+import React, { useEffect } from 'react';
+import { Container, Modal, Row } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
 import InvoiceHistory from '../../components/StudentComponents/Payments/InvoiceHistory';
 import CreatePaymentMethod from '../../components/StudentComponents/Payments/CreatePaymentMethod';
-import CreateSubscription from '../../components/StudentComponents/Payments/CreateSubscription';
-import CreateStripeAccount from '../../components/StudentComponents/Payments/CreateStripeAccount';
 import SubscriptionDetail from '../../components/StudentComponents/Payments/SubscriptionDetail';
-import ManagePaymentMethod from '../../components/StudentComponents/Payments/ManagePaymentMethod';  
+import ManagePaymentMethod from '../../components/StudentComponents/Payments/ManagePaymentMethod';
+import { useOktaAuth } from '@okta/okta-react';
+import { setAccessToken } from '../../service/axiosConfig';
+import { searchCustomersBySysIdThunk, fetchOneSubscriptionThunk } from "../../components/StudentComponents/Payments/PaymentsSlice";
+import { formatCurrency, formatDate } from "../../util/formatters";
 
 const Payments = () => {
     const [showModal, setShowModal] = React.useState(false);
+
+    const { authState, oktaAuth } = useOktaAuth();
+    const id = authState?.idToken?.claims?.sub;
+    const dispatch = useDispatch();
+    const customerAdapter = useSelector((state) => state.payments.entities["customer"]);
+    const customerData = customerAdapter?.entities[id];
+
+    const subscriptionId = useSelector((state) => state.payments?.subscriptionId);
+    const subscriptions = useSelector((state) => state.payments?.entities?.subscriptions);
+    const subscription = subscriptions?.entities[subscriptionId];
+
+    const loading = useSelector((state) => state.payments?.loading);
+
+    const plan = subscription?.items?.data[0]?.price?.productObject?.description;
+    const price = subscription?.items?.data[0]?.price?.unitAmount;
+
+    useEffect(() => {
+        const fetchCustomerData = async () => {
+            if (authState?.isAuthenticated && id && !customerData) {
+                try {
+                    setAccessToken(oktaAuth.getAccessToken());
+                    await dispatch(searchCustomersBySysIdThunk({ customerId: id })).unwrap();
+                } catch (error) {
+                    console.error("Error fetching customer data:", error);
+                }
+            }
+        };
+
+        fetchCustomerData();
+    }, [authState?.isAuthenticated, id, dispatch, oktaAuth, customerData]);
+
+    useEffect(() => {
+        const fetchSubscriptionData = async () => {
+            if (subscriptionId) {
+                try {
+                    setAccessToken(oktaAuth.getAccessToken());
+                    await dispatch(fetchOneSubscriptionThunk(subscriptionId)).unwrap();
+                } catch (error) {
+                    console.error("Error fetching subscription data:", error);
+                }
+            }
+        };
+
+        fetchSubscriptionData();
+    }, [subscriptionId, dispatch, oktaAuth]);
+
     return (
         <Container>
-            <Row>             
-                    <SubscriptionDetail /> 
-                    <InvoiceHistory />
-                    {/* <CreateSubscription />
+            <Row>
+                <SubscriptionDetail subscription={subscription} />
+                <InvoiceHistory />
+                {/* <CreateSubscription />
                     <CreateStripeAccount /> */}
-                    <ManagePaymentMethod />
+                <ManagePaymentMethod />
             </Row>
             <Modal show={showModal} onHide={() => setShowModal(false)}>
                 <Modal.Header closeButton>
                     <Modal.Title>Create Payment Method</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <CreatePaymentMethod showModal={showModal} setShowModal={setShowModal}/>
+                    <CreatePaymentMethod showModal={showModal} setShowModal={setShowModal} />
                 </Modal.Body>
             </Modal>
         </Container>
